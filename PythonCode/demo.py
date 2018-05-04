d9c5f356ea530cd7bf80219cd5d4a8878df82629
JiafuYuan <yuanjiafu2008@163.com>
2018-05-04 14:37:14 +0800

    demo学习

--- PythonCode/demo.py
+++ PythonCode/demo.py
@@ -1,10 +1,28 @@
-print("现在开始")
-temp=input("请输入大于5的数字")
-guest=int(temp)
-if guest>5:
-    print("正确")
-else:
-    print("错误")
-print("游戏结束")
+
+def newmethod744():
+    print("现在开始")
+    temp=input("请输入大于5的数字")
+    guest=int(temp)
+    if guest>5:
+        print("正确")
+    else:
+        print("错误")
+    print("游戏结束")
+
+# newmethod744()
+
+def zhengxingAdd():
+    first=1
+    second=2
+    third=first+second
+    print(third)
+
+def stringAdd():
+    first="c:\\now"
+    second="Let's go"
+    four=r"c:\now\fish"
+    third=first+second+four
+    print(third)
+stringAdd()
 
 
