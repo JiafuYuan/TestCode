3ded559a17366756cf4043368ab036706674bfd7
JiafuYuan <JiafuYuan@163.com>
2019-04-19 14:09:31 +0800

    js对象代码梳理

--- JsCode/javascript.js
+++ JsCode/javascript.js
@@ -1,2 +1,15 @@
 var s = "javascript";
 $("#log").html("javascript中v的indexof:" + s.indexOf("v"));
+
+var t={
+    init:function(data){
+alert("run init func with_"+data);
+    },
+    sum:s
+};
+t.sum=10;
+t.init(t.sum);
+var f=function(a,b){
+alert(a+b);
+};
+f(1,2);
